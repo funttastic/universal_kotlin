2915cf30d5d8fdc00aca687cc92205988f68381d
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-01-18 21:30:35 +0100

    Modifying xcode.sh

--- resources/scripts/apple/xcode.sh
+++ resources/scripts/apple/xcode.sh
@@ -6,44 +6,46 @@
 #   application/mobile/native/apple/ios/ios_x64_copying_framework/iosApp.xcodeproj/project.pbxproj
 #   application/mobile/native/apple/ios/ios_x64_framework/framework.xcodeproj/project.pbxproj
 
-printf "Environment Variables:\n"
-env
-
 if [[ -z "$module" ]]; then
     printf "Module name not found. Check if the 'shellScript' entry is correctly configured in your 'project.pbxproj' file."
 
     exit 1
 fi
 
-enabledModules="$module"
-task=":application:application-mobile:application-mobile-native:application-mobile-native-apple:application-mobile-native-apple-ios:$module"
+export enabledModules="$module"
+export task=":application:application-mobile:application-mobile-native:application-mobile-native-apple:application-mobile-native-apple-ios:$module"
 
-if [ "$module" = "ios_x64" ]; then
-    printf "Before - Contents of $TARGET_BUILD_DIR"
-    tree $TARGET_BUILD_DIR
+printf "Environment Variables:\n"
+env | sort
 
-    rm -f $TARGET_BUILD_DIR/$EXECUTABLE_PATH
+
+printf "Before - Contents of $TARGET_BUILD_DIR"
+tree $TARGET_BUILD_DIR
+
+if [ "$module" = "application-mobile-native-apple-ios-ios_x64" ]; then
+    echo "Skipping"
+    # rm -f $TARGET_BUILD_DIR/$EXECUTABLE_PATH
 fi
 
-if [ "$module" = "ios_x64_copying_framework" ]; then
+if [ "$module" = "application-mobile-native-apple-ios-ios_x64_copying_framework" ]; then
     ./gradlew --stacktrace --warning-mode all \
         -p $SRCROOT/ \
         -Pconfiguration.build.dir=$CONFIGURATION_BUILD_DIR \
         -Pkotlin.build.type=$KOTLIN_BUILD_TYPE \
         -Pkotlin.target=$KOTLIN_TARGET \
-        "$path:copyFramework"
+        "$task:copyFramework"
 elif [ -z $KOTLIN_NATIVE_BUILD_CAPABLE ]; then
-    ./gradlew --stacktrace --warning-mode all "$path:buildForXcode"
+    ./gradlew --stacktrace --warning-mode all "$task:buildForXcode"
 else
     printf "Uknown module '$module'. Not target action defined"
 
     exit 2
 fi
 
-if [ "$module" = "ios_x64" ]; then
-    mkdir -p $TARGET_BUILD_DIR/$TARGET_NAME.app
-    cp "$TARGET_BUILD_DIR/$module.kexe" $TARGET_BUILD_DIR/$EXECUTABLE_PATH
-
-    printf "After - Contents of $TARGET_BUILD_DIR"
-    tree $TARGET_BUILD_DIR
+if [ "$module" = "application-mobile-native-apple-ios-ios_x64" ]; then
+    mkdir -p $TARGET_BUILD_DIR/application.app
+    mv "$TARGET_BUILD_DIR/$module.kexe" $TARGET_BUILD_DIR/application.app/application
 fi
+
+printf "After - Contents of $TARGET_BUILD_DIR"
+tree $TARGET_BUILD_DIR
