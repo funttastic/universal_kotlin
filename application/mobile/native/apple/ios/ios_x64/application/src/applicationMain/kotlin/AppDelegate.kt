d93d2ad36dfecc9decf1d73ec12cdd716e7968cf
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-01-19 20:26:05 +0100

    Improving identation and cleaning code.

--- application/mobile/native/apple/ios/ios_x64/application/src/applicationMain/kotlin/AppDelegate.kt
+++ application/mobile/native/apple/ios/ios_x64/application/src/applicationMain/kotlin/AppDelegate.kt
@@ -2,22 +2,47 @@ import kotlinx.cinterop.*
 import platform.Foundation.*
 import platform.UIKit.*
 
+/**
+ *
+ */
 class AppDelegate : UIResponder, UIApplicationDelegateProtocol {
+
+	/**
+	 *
+	 */
 	companion object : UIResponderMeta(), UIApplicationDelegateProtocolMeta {}
 
+	/**
+	 *
+	 */
 	@OverrideInit
 	constructor() : super()
 
+	/**
+	 *
+	 */
 	private var _window: UIWindow? = null
+
+	/**
+	 *
+	 */
 	override fun window() = _window
+
+	/**
+	 *
+	 */
 	override fun setWindow(window: UIWindow?) {
 		_window = window
 	}
 
+	/**
+	 *
+	 */
 	override fun application(application: UIApplication, didFinishLaunchingWithOptions: Map<Any?, *>?): Boolean {
 		window = UIWindow(frame = UIScreen.mainScreen.bounds)
 		window!!.rootViewController = ViewController()
 		window!!.makeKeyAndVisible()
+
 		return true
 	}
 }
