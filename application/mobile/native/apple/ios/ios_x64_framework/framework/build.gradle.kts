cc98f6d2ee67981c0697268d47e2b3dd0480144a
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-01-19 21:22:42 +0100

    Cleaning and improving code.

--- application/mobile/native/apple/ios/ios_x64_framework/framework/build.gradle.kts
+++ application/mobile/native/apple/ios/ios_x64_framework/framework/build.gradle.kts
@@ -1,30 +1,22 @@
-import com.company.team.project.dsl.model.enum_.*
-import com.company.team.project.dsl.model.extension.*
+import com.company.team.project.dsl.model.enum_.ModuleEnum
+import com.company.team.project.dsl.model.extension.configureTargetAttributes
 
 plugins {
-    kotlin("multiplatform")
-    kotlin("xcode-compat")
+	kotlin("multiplatform")
+	kotlin("xcode-compat")
 }
 
 repositories {
-    mavenCentral()
-    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+	mavenCentral()
+	maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 kotlin {
 	configureTargetAttributes(ModuleEnum.`application-mobile-native-apple-ios-ios_x64_framework`)
 
-//	targets.all {
-//			compilations.all {
-//				tasks[compileKotlinTaskName].kotlinOptions {
-//					allWarningsAsErrors = true
-//				}
-//			}
-//	}
-
-    xcode {
-        setupFramework("framework") {
-            baseName = "framework"
-        }
-    }
+	xcode {
+		setupFramework("framework") {
+			baseName = "framework"
+		}
+	}
 }
