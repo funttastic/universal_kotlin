31baeb6f2fdfa7bb8281e363b9c2069687bef164
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-01-19 20:54:59 +0100

    Updating xcode script.

--- resources/scripts/apple/xcode.sh
+++ resources/scripts/apple/xcode.sh
@@ -7,9 +7,9 @@
 #   application/mobile/native/apple/ios/ios_x64_framework/framework.xcodeproj/project.pbxproj
 
 if [[ -z "$module" ]]; then
-    printf "Module name not found. Check if the 'shellScript' entry is correctly configured in your 'project.pbxproj' file."
+	printf "Module name not found. Check if the 'shellScript' entry is correctly configured in your 'project.pbxproj' file."
 
-    exit 1
+	exit 1
 fi
 
 export enabledModules="$module"
@@ -28,29 +28,29 @@ printf "$TARGET_BUILD_DIR\n"
 tree $TARGET_BUILD_DIR
 
 if [ "$module" = "application-mobile-native-apple-ios-ios_x64" ]; then
-    echo "Skipping"
-    # rm -f $TARGET_BUILD_DIR/$EXECUTABLE_PATH
+	 rm -f $TARGET_BUILD_DIR/application.app/application
 fi
 
 if [ "$module" = "application-mobile-native-apple-ios-ios_x64_copying_framework" ]; then
-    ./gradlew --stacktrace --warning-mode all \
-        -p $SRCROOT/ \
-        -Pconfiguration.build.dir=$CONFIGURATION_BUILD_DIR \
-        -Pkotlin.build.type=$KOTLIN_BUILD_TYPE \
-        -Pkotlin.target=$KOTLIN_TARGET \
-        "$task:copyFramework"
+	./gradlew --stacktrace --warning-mode all \
+		-p $SRCROOT/ \
+		-Pconfiguration.build.dir=$CONFIGURATION_BUILD_DIR \
+		-Pkotlin.build.type=$KOTLIN_BUILD_TYPE \
+		-Pkotlin.target=$KOTLIN_TARGET \
+		"$task:copyFramework"
 elif [ -z $KOTLIN_NATIVE_BUILD_CAPABLE ]; then
-    ./gradlew --stacktrace --warning-mode all "$task:buildForXcode"
+	./gradlew --stacktrace --warning-mode all "$task:buildForXcode"
 else
-    printf "Uknown module '$module'. Not target action defined"
+	printf "Uknown module '$module'. Not target action defined"
 
-    exit 2
+	exit 2
 fi
 
 if [ "$module" = "application-mobile-native-apple-ios-ios_x64" ]; then
-    # TODO Improve to use environment variable instead of hardcoded names
-    mkdir -p $TARGET_BUILD_DIR/application.app
-    mv "$TARGET_BUILD_DIR/application.kexe" $TARGET_BUILD_DIR/application.app/application
+	# TODO Improve to use environment variable instead of hardcoded names
+	mkdir -p $TARGET_BUILD_DIR/application.app
+	mv "$TARGET_BUILD_DIR/application.kexe" $TARGET_BUILD_DIR/application.app/application
+#	mv "$TARGET_BUILD_DIR/application.kexe" $TARGET_BUILD_DIR/application.app
 fi
 
 printf "TARGET_BUILD_DIR - Contents - After\n"
