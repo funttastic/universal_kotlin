438ed037a94ad026b9905cde0d4a2cbc0390e68d
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-01-26 16:59:20 +0100

    Improving build.gradle.kts.

--- application/mobile/native/apple/ios/ios_x64_with_framework/UniversalKotlin/build.gradle.kts
+++ application/mobile/native/apple/ios/ios_x64_with_framework/UniversalKotlin/build.gradle.kts
@@ -6,19 +6,6 @@ import com.company.team.project.dsl.model.extension.configureTargetAttributes
 import com.company.team.project.dsl.model.extension.iosX64
 import com.company.team.project.dsl.model.extension.setupFramework
 import com.company.team.project.dsl.model.extension.setupTask
-import org.gradle.api.DefaultTask
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.Sync
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.wrapper.Wrapper
-import org.gradle.kotlin.dsl.get
-import org.gradle.kotlin.dsl.task
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-
 
 plugins {
 	kotlin("multiplatform")
