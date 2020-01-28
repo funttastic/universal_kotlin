af3440ca7c7972b451d32378015662934cfb2368
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-01-28 20:07:31 +0100

    Updating Spring Boot controller.

--- application/backend/jvm/spring_boot/src/main/kotlin/com/company/team/project/application/backend/jvm/spring_boot/controller/ExampleController.kt
+++ application/backend/jvm/spring_boot/src/main/kotlin/com/company/team/project/application/backend/jvm/spring_boot/controller/ExampleController.kt
@@ -22,6 +22,6 @@ class ExampleController {
 	@RequestMapping("exampleMethod", method= [RequestMethod.GET])
 	@Throws(Exception::class)
 	fun example(): ResponseEntity<String> {
-		return ResponseEntity(exampleValue, HttpStatus.OK)
+		return ResponseEntity("<pre>$exampleValue</pre>", HttpStatus.OK)
 	}
 }
