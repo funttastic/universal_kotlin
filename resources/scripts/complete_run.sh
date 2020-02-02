66da663f661aa2cae58455501e929e721df45bfb
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-02-02 14:35:15 +0100

    Improving complete_run.sh script.

--- resources/scripts/complete_run.sh
+++ resources/scripts/complete_run.sh
@@ -1,67 +1,67 @@
 export enabledModules="common-multiple_sources"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :common-multiple_sources:build
+./gradlew --stacktrace :common-multiple_sources:build
 
 export enabledModules="common-single_source"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :common-single_source:build
+./gradlew --stacktrace :common-single_source:build
 
 export enabledModules="library-multiple_sources"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :library-multiple_sources:build
+./gradlew --stacktrace :library-multiple_sources:build
 
 export enabledModules="library-single_source"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :library-single_source:build
+./gradlew --stacktrace :library-single_source:build
 
 export enabledModules="application-backend-jvm-spring_boot"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-backend-jvm-spring_boot:build
+./gradlew --stacktrace :application-backend-jvm-spring_boot:build
 
 export enabledModules="application-browser-js-spa-react"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-browser-js-spa-react:build
+./gradlew --stacktrace :application-browser-js-spa-react:build
 
 export enabledModules="application-browser-js-vanilla"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-browser-js-vanilla:build
+./gradlew --stacktrace :application-browser-js-vanilla:build
 
 export enabledModules="application-browser-native-wasm32"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-browser-native-wasm32:build
+./gradlew --stacktrace :application-browser-native-wasm32:build
 
 export enabledModules="application-desktop-jvm-tornado_fx"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-desktop-jvm-tornado_fx:build
+./gradlew --stacktrace :application-desktop-jvm-tornado_fx:build
 
 export enabledModules="application-mobile-jvm-android"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-mobile-jvm-android:build
+./gradlew --stacktrace :application-mobile-jvm-android:build
 
 export enabledModules="application-mobile-native-apple-ios-ios_x64"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-mobile-native-apple-ios-ios_x64:build
+./gradlew --stacktrace :application-mobile-native-apple-ios-ios_x64:build
 
 export enabledModules="application-mobile-native-apple-ios-ios_x64_copying_framework"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-mobile-native-apple-ios-ios_x64_copying_framework:build
+./gradlew --stacktrace :application-mobile-native-apple-ios-ios_x64_copying_framework:build
 
 export enabledModules="application-mobile-native-apple-ios-ios_x64_framework"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-mobile-native-apple-ios-ios_x64_framework:build
+./gradlew --stacktrace :application-mobile-native-apple-ios-ios_x64_framework:build
 
 export enabledModules="application-mobile-native-apple-ios-ios_x64_with_framework"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-mobile-native-apple-ios-ios_x64_with_framework:build
+./gradlew --stacktrace :application-mobile-native-apple-ios-ios_x64_with_framework:build
 
 export enabledModules="application-script-jvm-script"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-script-jvm-script:build
+./gradlew --stacktrace :application-script-jvm-script:build
 
 export enabledModules="application-terminal-jvm-terminal"
 echo "TARGET MODULES '$enabledModules'"
-./gradlew :application-terminal-jvm-terminal:build
+./gradlew --stacktrace :application-terminal-jvm-terminal:build
 
 export enabledModules=""
 echo "TARGET MODULES '$enabledModules'"
-./gradlew build
+./gradlew --stacktrace build
