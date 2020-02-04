bae32be5991f268d0400a75f2bf918ab512a2a60
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-02-04 21:59:14 +0100

    Adding new files for the other new presets in the common multiple sources module.

---
+++ common/multiple_sources/src/tvosX64Main/kotlin/com/company/team/project/common/multiple_sources/tvos_x64/Example.kt
@@ -0,0 +1,37 @@
+package com.company.team.project.common.multiple_sources.tvos_x64
+
+import com.company.team.project.common.multiple_sources.common.formatOutput
+
+/**
+ *
+ */
+val exampleValue = formatOutput(exampleFunction())
+
+/**
+ *
+ */
+fun exampleFunction(): List<String> {
+	return ExampleObject.exampleMethod()
+}
+
+/**
+ *
+ */
+object ExampleObject {
+
+	/**
+	 *
+	 */
+	private const val exampleProperty = "com.company.team.project.common.multiple_sources.tvos_x64"
+
+	/**
+	 *
+	 */
+	fun exampleMethod(): List<String> {
+		val output = ArrayList<String>()
+		output.addAll(com.company.team.project.common.multiple_sources.common.exampleFunction())
+		output.add(exampleProperty)
+
+		return output
+	}
+}
