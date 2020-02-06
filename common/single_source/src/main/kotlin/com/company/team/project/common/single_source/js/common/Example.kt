1933d62d3aea5a5dfe1b37740b2048fff96f4a82
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-02-06 21:39:41 +0100

    Impvoing single source js module.

--- common/single_source/src/main/kotlin/com/company/team/project/common/single_source/js/common/Example.kt
+++ common/single_source/src/main/kotlin/com/company/team/project/common/single_source/js/common/Example.kt
@@ -37,49 +37,49 @@ object ExampleObject {
 	}
 }
 
-///**
-// * The `_` value is where kotlin put the js packages.
-// * 	This value is defined both in the test module and in the main module, so they will have
-// * 	different values depending on it is being called.
-// */
-//@JsName("_")
-//external val `_`: dynamic
-//
-///**
-// * Since with Kotlin Js is not possible to extract the package of a class we did this workaround just to check
-// * 	if the package is ok.
-// */
-//fun checkAndReturnPackage(package_: String, class_: KClass<*>): String {
-//	if (
-//		class_.js.name
-//		=== eval("get_js(Kotlin.getKClassFromExpression(_.$package_.${class_.simpleName})).name")
-//	) {
-//		return package_
-//	}
-//
-//	throw IllegalArgumentException(
-//		"""
-//    	The package $package_ isn't the correct package for the class $class_.
-//    """
-//	)
-//}
-//
-///**
-// * Since with Kotlin Js is not possible to extract the package of a class, we did this workaround to check
-// * 	if the package is ok and return it. Otherwise this method throws an exception.
-// */
-//fun checkExampleObjectPackageAndReturnIt(): String {
-//	val expectedPackage = "com.company.team.project.common.multiple_sources.js"
-//	if (
-//		ExampleObject::class.js.name
-//		=== `_`.com.company.team.project.common.multiple_sources.js.ExampleObject::class.js.name
-//	) {
-//		return expectedPackage
-//	}
-//
-//	throw IllegalArgumentException(
-//		"""
-//    	The "$expectedPackage" isn't the correct package for the class "${ExampleObject::class.simpleName}".
-//    """
-//	)
-//}
+/**
+ * The `_` value is where kotlin put the js packages.
+ * 	This value is defined both in the test module and in the main module, so they will have
+ * 	different values depending on it is being called.
+ */
+@JsName("_")
+external val `_`: dynamic
+
+/**
+ * Since with Kotlin Js is not possible to extract the package of a class we did this workaround just to check
+ * 	if the package is ok.
+ */
+fun checkAndReturnPackage(package_: String, class_: KClass<*>): String {
+	if (
+		class_.js.name
+		=== eval("get_js(Kotlin.getKClassFromExpression(_.$package_.${class_.simpleName})).name")
+	) {
+		return package_
+	}
+
+	throw IllegalArgumentException(
+		"""
+    	The package $package_ isn't the correct package for the class $class_.
+    """
+	)
+}
+
+/**
+ * Since with Kotlin Js is not possible to extract the package of a class, we did this workaround to check
+ * 	if the package is ok and return it. Otherwise this method throws an exception.
+ */
+fun checkExampleObjectPackageAndReturnIt(): String {
+	val expectedPackage = "com.company.team.project.common.single_source.js.common"
+	if (
+		ExampleObject::class.js.name
+		=== `_`.com.company.team.project.common.single_source.js.common.ExampleObject::class.js.name
+	) {
+		return expectedPackage
+	}
+
+	throw IllegalArgumentException(
+		"""
+    	The "$expectedPackage" isn't the correct package for the class "${ExampleObject::class.simpleName}".
+    """
+	)
+}
