1411baddc3c9a325c3439449dac34d67fef6cfe5
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-02-08 16:17:39 +0100

    Testing CI configuration files.

--- resources/scripts/ci/prepare.sh
+++ resources/scripts/ci/prepare.sh
@@ -1,6 +1,6 @@
-#set -e
-#trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-#trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 echo "CI: $BUILD_CI"
 echo "OS: $BUILD_OS"
@@ -38,13 +38,16 @@ if [ "$BUILD_CI" == "APPVEYOR" ]; then
 	printf "Installing KScript:\n"
 	sdk install kscript
 
-#	printf "Installing Android SDK:\n"
-#	RMDIR "%ProgramFiles(x86)%\\Android\\android-sdk" /S /Q
-#	appveyor DownloadFile https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip
-#	7z x sdk-tools-windows-4333796.zip -o"%ProgramFiles(x86)%\\Android\\android-sdk" > nul
-#	echo off
-#	yes | "%ProgramFiles(x86)%\\Android\\android-sdk\\tools\\bin\\sdkmanager.bat" --licenses
-#	echo on
+	printf "Installing Android SDK:\n"
+	RMDIR "%ProgramFiles(x86)%\\Android\\android-sdk" /S /Q
+	appveyor DownloadFile https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip
+	7z x sdk-tools-windows-4333796.zip -o"%ProgramFiles(x86)%\\Android\\android-sdk" > nul
+	echo off
+	yes | "%ProgramFiles(x86)%\\Android\\android-sdk\\tools\\bin\\sdkmanager.bat" --licenses
+	echo on
+
+	printf "Installing Gradle:\n"
+	./gradlew --stacktrace --version
 elif [ "$BUILD_CI" == "CIRCLE_CI" ]; then
 	printf "Updating package manager:\n"
 	apt update
@@ -73,10 +76,13 @@ elif [ "$BUILD_CI" == "CIRCLE_CI" ]; then
 	printf "Installing KScript:\n"
 	sdk install kscript
 
-#	printf "Installing Android SDK:\n"
-#	curl -L https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -o $HOME/tools.zip
-#	unzip -q $HOME/tools.zip -d $ANDROID_HOME
-#	(echo y; echo y; echo y; echo y; echo y; echo y; echo y) | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+	printf "Installing Android SDK:\n"
+	curl -L https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -o $HOME/tools.zip
+	unzip -q $HOME/tools.zip -d $ANDROID_HOME
+	(echo y; echo y; echo y; echo y; echo y; echo y; echo y) | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+	printf "Installing Gradle:\n"
+	./gradlew --stacktrace --version
 elif [ "$BUILD_CI" == "TRAVIS" ]; then
 	printf "Updating package manager:\n"
 	echo "Skipping upgrade."
@@ -101,16 +107,21 @@ elif [ "$BUILD_CI" == "TRAVIS" ]; then
 	source "$HOME/.sdkman/bin/sdkman-init.sh"
 	sdk version
 
-#	printf "Installing Java $JAVA_VERSION:\n"
+	printf "Installing Java $JAVA_VERSION:\n"
+	set +e # In this CI the Repackaging command was failing without a reason.
 	sdk install java $JAVA_VERSION
+	set -e
 
 	printf "Installing KScript:\n"
 	sdk install kscript
 
-#	printf "Installing Android SDK:\n"
-#	curl -L https://dl.google.com/android/repository/sdk-tools-darwin-4333796.zip -o $HOME/tools.zip
-#	unzip -q $HOME/tools.zip -d $ANDROID_HOME
-#	yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+	printf "Installing Android SDK:\n"
+	curl -L https://dl.google.com/android/repository/sdk-tools-darwin-4333796.zip -o $HOME/tools.zip
+	unzip -q $HOME/tools.zip -d $ANDROID_HOME
+	yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+
+	printf "Installing Gradle:\n"
+	./gradlew --stacktrace --version
 else
 	printf "Unrecognized CI $BUILD_CI with OS $BUILD_OS.\n"
 fi
