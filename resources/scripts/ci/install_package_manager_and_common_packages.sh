bb952eadb84238ccb7d15f6e9ed27c219291ecb3
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-02-08 14:14:05 +0100

    Testing CI scripts.

--- resources/scripts/ci/install_package_manager_and_common_packages.sh
+++ resources/scripts/ci/install_package_manager_and_common_packages.sh
@@ -4,10 +4,23 @@ echo "OS: $BUILD_OS"
 echo "Installing and updating system packages:"
 if [ "$BUILD_CI" == "APPVEYOR" ]; then
 	choco upgrade chocolatey
+	choco install nvm
+
+	curl -s "https://get.sdkman.io" | bash
+	source "$HOME/.sdkman/bin/sdkman-init.sh"
+	sdk version
 elif [ "$BUILD_CI" == "CIRCLE_CI" ]; then
 	apt-get update && apt install unzip zip curl sed -y
+
+	curl -s "https://get.sdkman.io" | bash
+	source "$HOME/.sdkman/bin/sdkman-init.sh"
+	sdk version
 elif [ "$BUILD_CI" == "TRAVIS" ]; then
-	brew install unzip zip curl sed
+#	brew install unzip zip curl sed
+
+	curl -s "https://get.sdkman.io" | bash
+	source "$HOME/.sdkman/bin/sdkman-init.sh"
+	sdk version
 else
 	echo "Unrecognized CI $BUILD_CI with OS $BUILD_OS."
 fi
