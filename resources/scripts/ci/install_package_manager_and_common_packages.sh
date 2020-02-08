1e1b9207a3183b3de613b0d30f5ce0d74631a327
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-02-08 14:31:52 +0100

    Backing up files.

--- resources/scripts/ci/install_package_manager_and_common_packages.sh
+++ resources/scripts/ci/install_package_manager_and_common_packages.sh
@@ -1,28 +1,87 @@
 echo "CI: $BUILD_CI"
 echo "OS: $BUILD_OS"
 
-echo "Installing and updating system packages:"
+JAVA_VERSION="8.0.232.fx-zulu"
+
 if [ "$BUILD_CI" == "APPVEYOR" ]; then
+	echo "Updating package manager:"
 	choco upgrade chocolatey
+
+	echo "Installing common packages from the package manager:"
 	choco install nvm zip
 
+	echo "Installing SDKMAN!:"
 	curl -s "https://get.sdkman.io" | bash
 	source "$HOME/.sdkman/bin/sdkman-init.sh"
 	sdk version
+
+	echo "Installing Java $JAVA_VERSION:"
+	sdk install java $JAVA_VERSION
+
+	echo "Installing KScript:"
+	sdk install kscript
+
+	echo "Installing Android SDK:"
+	RMDIR "%ProgramFiles(x86)%\\Android\\android-sdk" /S /Q
+	appveyor DownloadFile https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip
+	7z x sdk-tools-windows-4333796.zip -o"%ProgramFiles(x86)%\\Android\\android-sdk" > nul
+	echo off
+	yes | "%ProgramFiles(x86)%\\Android\\android-sdk\\tools\\bin\\sdkmanager.bat" --licenses
+	echo on
 elif [ "$BUILD_CI" == "CIRCLE_CI" ]; then
+	echo "Updating package manager:"
 	apt update
+
+	echo "Installing common packages from the package manager:"
 	apt install unzip zip curl sed -y
 
+	echo "Installing SDKMAN!:"
 	curl -s "https://get.sdkman.io" | bash
 	source "$HOME/.sdkman/bin/sdkman-init.sh"
 	sdk version
+
+	echo "Installing Java $JAVA_VERSION:"
+	sdk install java $JAVA_VERSION
+
+	echo "Installing KScript:"
+	sdk install kscript
+
+	echo "Installing Android SDK:"
+	curl -L https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -o $HOME/tools.zip
+	unzip -q $HOME/tools.zip -d $ANDROID_HOME
+	(echo y; echo y; echo y; echo y; echo y; echo y; echo y) | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 elif [ "$BUILD_CI" == "TRAVIS" ]; then
+	echo "Updating package manager:"
 	brew upgrade
+
+	echo "Installing common packages from the package manager:"
 #	brew install unzip zip curl sed
 
+	echo "Installing SDKMAN!:"
 	curl -s "https://get.sdkman.io" | bash
 	source "$HOME/.sdkman/bin/sdkman-init.sh"
 	sdk version
+
+	echo "Installing Java $JAVA_VERSION:"
+	sdk install java $JAVA_VERSION
+
+	echo "Installing KScript:"
+	sdk install kscript
+
+	echo "Installing Android SDK:"
+	curl -L https://dl.google.com/android/repository/sdk-tools-darwin-4333796.zip -o $HOME/tools.zip
+	unzip -q $HOME/tools.zip -d $ANDROID_HOME
+	yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 else
 	echo "Unrecognized CI $BUILD_CI with OS $BUILD_OS."
 fi
+
+echo "Java:"
+java -version
+
+echo "Gradle:"
+./gradlew --version
+
+echo "Environment Variables:"
+env
+
