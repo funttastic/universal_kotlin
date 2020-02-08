6e691f31d7c56a7e29b86fe781dfb5291be329ec
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-02-08 15:58:37 +0100

    Testing CI configuration files.

--- resources/scripts/ci/prepare.sh
+++ resources/scripts/ci/prepare.sh
@@ -1,6 +1,6 @@
 set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+#trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 echo "CI: $BUILD_CI"
 echo "OS: $BUILD_OS"
@@ -15,9 +15,10 @@ if [ "$BUILD_CI" == "APPVEYOR" ]; then
 	choco upgrade chocolatey
 
 	printf "Installing common packages from the package manager:\n"
-	choco install zip nvm
+	choco install zip nvm tree
 
 	printf "Installing NVM:\n"
+	tree ~
 	cmd "/C refreshenv"
 
 	printf "Installing latest NodeJS:\n"
@@ -37,13 +38,13 @@ if [ "$BUILD_CI" == "APPVEYOR" ]; then
 	printf "Installing KScript:\n"
 	sdk install kscript
 
-	printf "Installing Android SDK:\n"
-	RMDIR "%ProgramFiles(x86)%\\Android\\android-sdk" /S /Q
-	appveyor DownloadFile https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip
-	7z x sdk-tools-windows-4333796.zip -o"%ProgramFiles(x86)%\\Android\\android-sdk" > nul
-	echo off
-	yes | "%ProgramFiles(x86)%\\Android\\android-sdk\\tools\\bin\\sdkmanager.bat" --licenses
-	echo on
+#	printf "Installing Android SDK:\n"
+#	RMDIR "%ProgramFiles(x86)%\\Android\\android-sdk" /S /Q
+#	appveyor DownloadFile https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip
+#	7z x sdk-tools-windows-4333796.zip -o"%ProgramFiles(x86)%\\Android\\android-sdk" > nul
+#	echo off
+#	yes | "%ProgramFiles(x86)%\\Android\\android-sdk\\tools\\bin\\sdkmanager.bat" --licenses
+#	echo on
 elif [ "$BUILD_CI" == "CIRCLE_CI" ]; then
 	printf "Updating package manager:\n"
 	apt update
@@ -72,10 +73,10 @@ elif [ "$BUILD_CI" == "CIRCLE_CI" ]; then
 	printf "Installing KScript:\n"
 	sdk install kscript
 
-	printf "Installing Android SDK:\n"
-	curl -L https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -o $HOME/tools.zip
-	unzip -q $HOME/tools.zip -d $ANDROID_HOME
-	(echo y; echo y; echo y; echo y; echo y; echo y; echo y) | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+#	printf "Installing Android SDK:\n"
+#	curl -L https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -o $HOME/tools.zip
+#	unzip -q $HOME/tools.zip -d $ANDROID_HOME
+#	(echo y; echo y; echo y; echo y; echo y; echo y; echo y) | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 elif [ "$BUILD_CI" == "TRAVIS" ]; then
 	printf "Updating package manager:\n"
 	echo "Skipping upgrade."
@@ -106,10 +107,10 @@ elif [ "$BUILD_CI" == "TRAVIS" ]; then
 	printf "Installing KScript:\n"
 	sdk install kscript
 
-	printf "Installing Android SDK:\n"
-	curl -L https://dl.google.com/android/repository/sdk-tools-darwin-4333796.zip -o $HOME/tools.zip
-	unzip -q $HOME/tools.zip -d $ANDROID_HOME
-	yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+#	printf "Installing Android SDK:\n"
+#	curl -L https://dl.google.com/android/repository/sdk-tools-darwin-4333796.zip -o $HOME/tools.zip
+#	unzip -q $HOME/tools.zip -d $ANDROID_HOME
+#	yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
 else
 	printf "Unrecognized CI $BUILD_CI with OS $BUILD_OS.\n"
 fi
