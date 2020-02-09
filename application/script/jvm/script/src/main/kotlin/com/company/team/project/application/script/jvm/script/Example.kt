bb008922268d2c4fe5ff613fabdcde673b1822c1
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-02-09 15:42:36 +0100

    Fixing KScript example to avoid warning that incurrs in error.

--- application/script/jvm/script/src/main/kotlin/com/company/team/project/application/script/jvm/script/Example.kt
+++ application/script/jvm/script/src/main/kotlin/com/company/team/project/application/script/jvm/script/Example.kt
@@ -2,9 +2,13 @@
 @file:DependsOnMaven("com.company.team.project:library-multiple_sources-jvm:0.0.1")
 @file:DependsOnMaven("com.company.team.project:library-single_source-jvm:0.0.1")
 
+// Avoid the error "... is compiled by a pre-release version of Kotlin and cannot be loaded by this version of the compiler"
+@file:CompilerOpts("-Xskip-metadata-version-check")
+
 package com.company.team.project.application.script.jvm.script
 
 import DependsOnMaven
+import CompilerOpts
 import com.company.team.project.common.single_source.common.OutputType.TERMINAL
 import com.company.team.project.common.single_source.common.formatOutput
 import org.jline.terminal.TerminalBuilder
