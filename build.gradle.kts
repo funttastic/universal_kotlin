688f20a82034c29bad4d94bb73fc4c53bfeac78f
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2020-05-12 22:15:33 -0300

    Updating build.gradle.kts of the root module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,17 @@ import com.company.team.project.dsl.generateGraphvizDiagrams
 import com.company.team.project.dsl.model.enum_.StatusEnum.enabled
 
 buildscript {
+	apply("plugin/properties.gradle.kts")
+	apply("plugin/dependencies.gradle.kts")
+	@Suppress("unchecked_cast", "nothing_to_inline")
+	fun <T> uncheckedCast(target: Any?): T = target as T
+	val getProperty = uncheckedCast<(key: String) -> String>(extra["getProperty"])
+	val getDependencyVersion = uncheckedCast<(group: String, dependencyId: String) -> String>(extra["getDependencyVersion"])
+
+	// The full package is needed here.
 	com.company.team.project.dsl.model.Properties.projects.root.project = rootProject
 
-	val vendorProperties = com.company.team.project.dsl.model.Properties.vendor
+	val kotlinVersion = getProperty("kotlin.version")
 
 	repositories {
 		flatDir { dirs("plugin") }
@@ -30,40 +38,34 @@ buildscript {
 	}
 
 	dependencies {
-		classpath("com.android.tools.build:gradle:${vendorProperties.androidTools}")
-		classpath("org.springframework.boot:spring-boot-gradle-plugin:${vendorProperties.springBoot}")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${vendorProperties.kotlin}")
-		classpath("org.jetbrains.kotlin:kotlin-allopen:${vendorProperties.kotlin}")
-		classpath("org.jetbrains.kotlin:kotlin-frontend-plugin:${vendorProperties.frontend}")
-		classpath("org.jetbrains.kotlin:kotlin-serialization:${vendorProperties.kotlin}")
-//		classpath("org.jetbrains.kotlin:kotlin-native-gradle-plugin:${vendorProperties.kotlin}")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+		classpath("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
+		classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
 
-		classpath("com.github.salomonbrys.gradle.kotlin.js:kotlin-js-gradle-utils:1.2.0")
-
+		// For Terminal modules
 		classpath("com.github.jengelman.gradle.plugins:shadow:5.0.0")
 
-//		classpath("no.tornado:fxlauncher-gradle-plugin:1.0.20")
-//		classpath("de.dynamicfiles.projects.gradle.plugins:javafx-gradle-plugin:8.8.2")
-
-		// TODO Fix
-//		classpath ("com.company.team.project:plugin-jvm-gradle")
+		// For Spring Boot modules
+		classpath("org.springframework.boot:spring-boot-gradle-plugin:2.2.2.RELEASE")
 
 		// For Android modules
+		classpath("com.android.tools.build:gradle:3.3.2")
 		classpath("com.android.tools.build:gradle:3.5.3")
-		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${vendorProperties.kotlin}")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+
+		// For JavaScript modules
+		classpath("org.jetbrains.kotlin:kotlin-frontend-plugin:0.0.45")
+		classpath("com.github.salomonbrys.gradle.kotlin.js:kotlin-js-gradle-utils:1.2.0")
+
+		// TODO Add reference in the future
+//		classpath ("com.company.team.project:plugin-jvm-gradle")
 	}
 }
 
 Util.initialize(gradle.startParameter.taskNames)
 
-// TODO Fix
+// TODO Add reference in the future
 //apply(plugin = "com.company.team.project:plugin-jvm-gradle")
-
-//group = Properties.general.group
-//version = Properties.general.version
-
-//version = ext["project.version"]!!
-//group = ext["project.group"]!!
 
 allprojects {
 	repositories {
@@ -78,73 +80,18 @@ allprojects {
 		maven( url = "https://dl.bintray.com/kotlin/kotlin-eap" )
 		// maven( url = "https://dl.bintray.com/kotlin/kotlin-dev" )
 	}
-
-	group = Properties.general.group
-	version = Properties.general.version
-
-//	version = ext["project.version"]!!
-//	group = ext["project.group"]!!
-
-//	ext {
-//		set("property", "value")
-//	}
-
-//	// Workaround to avoid downloading dependencies every time.
-//	repositories {
-//		maven {
-//			url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
-//			content {
-////				excludeGroup("Kotlin/Native")
-//			}
-//		}
-//		mavenLocal().apply {
-//			content {
-////				excludeGroup("Kotlin/Native")
-//			}
-//		}
-//		maven {
-//			url = uri("https://dl.bintray.com/soywiz/soywiz")
-//			content {
-//				includeGroup("com.soywiz")
-////				excludeGroup("Kotlin/Native")
-//			}
-//		}
-//		jcenter() {
-//			content {
-////				excludeGroup("Kotlin/Native")
-//			}
-//		}
-//		google().apply {
-//			content {
-////				excludeGroup("Kotlin/Native")
-//			}
-//		}
-//	}
 }
 
-rootProject.allprojects.forEach { project ->
-	// Force initialization of the module property
-	if (project.module.status != enabled) {
-		project.tasks.all {
+subprojects {
+	// Also force the initialization of the module property
+	if (this.module.status != enabled) {
+		this.tasks.all {
 			this.enabled = false
 		}
 	}
 }
 
-repositories {
-	flatDir { dirs("plugin") }
-	mavenLocal()
-	gradlePluginPortal()
-	mavenCentral()
-	jcenter()
-	google()
-	maven( url = "https://plugins.gradle.org/m2" )
-	maven( url = "https://repo.gradle.org/gradle/libs-releases-local" )
-	maven( url = "https://dl.bintray.com/kotlin/kotlin-eap" )
-	// maven( url = "https://dl.bintray.com/kotlin/kotlin-dev" )
-}
-
-tasks.register("generateGraphvizDiagrams") {
+task("generateGraphvizDiagrams") {
 	doLast {
 		generateGraphvizDiagrams()
 	}
